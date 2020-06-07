b907fa286de8ca8d959d670e529378abc501d8d0
fernandopassaia <fernandopassaia@futuradata.com.br>
2020-06-07 17:24:57 +0100

    Going Back to Win

--- src/BackEnd/AppFullStackDemo.Infra/Context/AppFullStackDemoContext.cs
+++ src/BackEnd/AppFullStackDemo.Infra/Context/AppFullStackDemoContext.cs
@@ -38,7 +38,7 @@ namespace AppFullStackDemo.Infra.Context
         {
             if (!optionsBuilder.IsConfigured) //if it's not configured and not comming from anysource, I'll use the default connection here
             {
-                optionsBuilder.UseSqlServer("Data Source=172.19.0.2,1433;Initial Catalog=AppFullStackDemo;Persist Security Info=True;User ID=sa;Password=@1234Fd#");
+                optionsBuilder.UseSqlServer("Data Source=172.18.0.2,4002;Initial Catalog=AppFullStackDemo;Persist Security Info=True;User ID=sa;Password=@1234Fd#");
             }
         }
 
