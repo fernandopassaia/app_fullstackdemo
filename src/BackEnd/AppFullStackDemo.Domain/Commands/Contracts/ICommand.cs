d400a7226fe6f59e68543abbb4e18025191d6b43
fernandopassaia <fernandopassaia@futuradata.com.br>
2020-07-09 22:45:05 +0100

    updates on docs

--- src/BackEnd/AppFullStackDemo.Domain/Commands/Contracts/ICommand.cs
+++ src/BackEnd/AppFullStackDemo.Domain/Commands/Contracts/ICommand.cs
@@ -5,5 +5,6 @@ namespace AppFullStackDemo.Domain.Commands.Contracts
     public interface ICommand : IValidatable
     {
         // ICommand (and who implement it) will have a Validate() method (from IValidatable) to implement (and validate the DTO)
+        // void Validate(); //I'll comment this validate method because this class is inheritancing from IValidatable yet...
     }
 }
--- src/BackEnd/AppFullStackDemo.Domain/Handlers/Contracts/IHandler.cs
+++ src/BackEnd/AppFullStackDemo.Domain/Handlers/Contracts/IHandler.cs
@@ -3,6 +3,8 @@ using AppFullStackDemo.Domain.Results;
 
 namespace AppFullStackDemo.Domain.Handlers.Contracts
 {
+    // This the contract for Handler: Handler could be <T>, since T (the handler) implement ICommand
+    // So basically all the commands should implement ICommand
     public interface IHandler<T> where T : ICommand
     {
         IBaseCommandResult Handle(T command);
